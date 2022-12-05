054cdfc577f8182f823e04852480392d3a090dbe
GGGDunlix <jishy710@gmail.com>
2022-12-04 20:23:26 -0500

    final version fixes

--- FivePD P LockerRoomEUP/Class1.cs
+++ FivePD P LockerRoomEUP/Class1.cs
@@ -29,22 +29,9 @@ namespace FivePDEUPLocker
                     bool markerStatus = (bool)locConfig["marker"];
                     string tooltip = locConfig["tooltip"].ToString();
                     Debug.WriteLine("FivePD EUP Locker: JSON Files loaded, no errors found...");
-                    foreach (JObject _locConfig in locations)
-                    {
-                        var _posJSON = _locConfig["coords"];
-                        Vector3 _position = new Vector3((float)_posJSON["x"], (float)_posJSON["y"], (float)_posJSON["z"]);
-                        bool _blipStatus = (bool)_locConfig["blip"];
-                        bool _markerStatus = (bool)_locConfig["marker"];
-                        string _tooltip = _locConfig["tooltip"].ToString();
-
-                        if (blipStatus)
-                        {
-                            Blip cBlip = new Blip(API.AddBlipForCoord(position.X, position.Y, position.Z));
-                            cBlip.Color = BlipColor.Blue;
-                            cBlip.Sprite = BlipSprite.Clothes;
-                        }
-                    }
-                        Tick += isNear;
+                    spawnBlips();
+                    Debug.WriteLine("FivePD EUP Locker: Spawning Blips");
+                    Tick += isNear;
                 } catch (Exception ex)
                 {
                     Debug.WriteLine("FivePD EUP Locker: Error in the coordinates.json: " + ex);
@@ -52,7 +39,30 @@ namespace FivePDEUPLocker
 
                 }
             }
+            public async Task spawnBlips()
+            {
+                var json = API.LoadResourceFile(API.GetCurrentResourceName(), "/config/coordinates.json");
+                var config = JObject.Parse(json);
+                var locations = config["LockerEUP"]["locations"];
+                foreach (JObject locConfig in locations)
+                {
+                    var posJSON = locConfig["coords"];
+                    Vector3 position = new Vector3((float)posJSON["x"], (float)posJSON["y"], (float)posJSON["z"]);
+                    bool blipStatus = (bool)locConfig["blip"];
 
+                    if (blipStatus)
+                    {
+                        if (Game.PlayerPed.IsInRangeOf(position, 25f))
+                        {
+
+                            Blip cBlip = new Blip(API.AddBlipForCoord(position.X, position.Y, position.Z));
+                            cBlip.Color = BlipColor.Blue;
+                            cBlip.Sprite = BlipSprite.Clothes;
+
+                        }
+                    }
+                }
+            }
             public async Task isNear()
             {
                 {
